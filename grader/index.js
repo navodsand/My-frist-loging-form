603cc2a8fd563905a74495f116f9af6b2bdffd55
navodsand <sandaruwannavod223@gmail.com>
2022-09-27 22:12:48 +0530

    first commit

---
+++ grader/index.js
@@ -0,0 +1,37 @@
+
+var grade;
+var mark = prompt("INPUT MARK");
+
+if (0 > mark) {
+    grade = "Mark Is invalid";
+    alert(grade);
+  } 
+  else if (0 <= mark < 35) {
+    grade = "Grade Is w";
+    alert(grade);
+  }
+  else if (35 <= mark < 55) {
+    grade = "Grade Is S";
+    alert(grade);
+  }
+   else if (55 <= mark < 65) {
+    grade = "Grade Is C";
+    alert(grade);
+  }
+  else if (65 <= mark < 75) {
+    grade = "Grade Is B";
+    alert(grade);
+  }
+  else if (75 <= mark <= 100) {
+    grade = "Grade Is A";
+    alert(grade);
+  }
+  else if (100 < mark) {
+    grade = "Mark Is Invalid";
+    alert(grade);
+  }
+else
+
+
+
+alert(grade);
